64eaf746443b41cb527e8e8674e6f25e0be8f0b8
svetterIO <2234705+svetterIO@users.noreply.github.com>
2025-11-06 13:35:44 +0100

    CleanUp logHeader

--- src/main/java/burp/EditorTab.java
+++ src/main/java/burp/EditorTab.java
@@ -2,17 +2,12 @@ package burp;
 
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
-import burp.api.montoya.ui.settings.SettingsPanel;
 
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableColumn;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.InputStream;
 import java.util.*;
-import java.util.List;
-import java.util.regex.Pattern;
+
 
 public class EditorTab implements BurpExtension {
 
@@ -30,22 +25,9 @@ public class EditorTab implements BurpExtension {
 
         // Option B: settings panel
         api.userInterface().registerSettingsPanel(new TokenSettingsPanel(api));
-
-        logHeader(api);
     }
 
-    private void logHeader(MontoyaApi api) {
-        api.logging().logToOutput("====================================================");
-        api.logging().logToOutput(" Project Information");
-        api.logging().logToOutput("====================================================");
-        api.logging().logToOutput(" Author       : Philipp Röder");
-        api.logging().logToOutput(" Contributors : Sebastian Vetter, Kartik Rastogi");
-        api.logging().logToOutput(" Version      : " + loadVersion());
-        api.logging().logToOutput("====================================================");
-    }
-
-
-    private String loadVersion() {
+    private String loadVersion(MontoyaApi api) {
         try (InputStream input = getClass().getClassLoader().getResourceAsStream("version.properties")) {
             if (input != null) {
                 Properties props = new Properties();
@@ -73,7 +55,4 @@ public class EditorTab implements BurpExtension {
 
     /* ===== Types moved here so they're available everywhere ===== */
     public enum Colour { RED, GREEN, BLUE, YELLOW, MAGENTA, CYAN, ORANGE, GRAY }
-
-
-
 }
